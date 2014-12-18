40671494b6ea936d218878ebf5ccd747e8ed954d
Natnapin Ketchaikosol <natnapinketch@Natnapins-MacBook-Air.local>
2014-12-18 10:52:24 +0700

    Initial Commit

---
+++ University Road/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  University Road
+//
+//  Created by Natnapin Ketchaikosol on 12/18/2557 BE.
+//  Copyright (c) 2557 Natnapin K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
